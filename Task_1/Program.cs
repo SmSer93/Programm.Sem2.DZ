07db3480cef3f538c464d0632bee51d935dc2dd9
Sergey <segarik93@gmail.com>
2022-11-21 00:04:50 +0300

    DZ Sem2.Task1.End

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a third-digit number:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1000 && number > 99)
+{ 
+    number = number /10 % 10;
+    Console.WriteLine($"Second digit --> {number}" );
+}
