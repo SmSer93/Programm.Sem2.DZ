0b3ba46c3259164150f03355e3733793c9c5e144
Sergey <segarik93@gmail.com>
2022-11-21 01:26:46 +0300

    DZ Sem2.Task2.End

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) Console.WriteLine("The third digit is missing");
+while (number > 1000) number = number / 10;  
+if (number < 1000 && number > 99)
+{
+number = number % 10;
+Console.WriteLine($"Third digit --> {number}" );
+}
+     
+    
+        
+
+
