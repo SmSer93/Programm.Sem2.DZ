813778ac081d5eacb6c54ab9e8db8df7a13f40b2
Sergey <segarik93@gmail.com>
2022-11-21 22:41:34 +0300

    DZ Sem2.Task3.End

---
+++ Task_3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter a number for the day of the week: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <=5)  Console.WriteLine("Нет");
+else Console.WriteLine("Да");
